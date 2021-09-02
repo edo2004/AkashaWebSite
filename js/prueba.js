db689b7e0a40ce5a3c60d4051145fb881814f5c6
Eduar <eduarord@unicauca.edu.co>
2021-09-02 14:06:01 -0500

    hexagonos galería

--- js/prueba.js
+++ js/prueba.js
@@ -1,16 +1,21 @@
-document.querySelector('.fondo3').style.display = "none"
-document.querySelector('.fondo2').style.display = "none"
-document.querySelector('.button').addEventListener('click', function () {
-    document.querySelector('.fondo2').style.display = ""
+const fondo1 = document.querySelector('.fondo1')
+const fondo2 = document.querySelector('.fondo2')
+const fondo3 = document.querySelector('.fondo3')
+const button = document.querySelector('.button')
+
+
+fondo3.style.display = "none"
+fondo2.style.display = "none"
+button.addEventListener('click', function () {
+    fondo2.style.display = ""
     setTimeout(function() {
-        document.querySelector('.fondo2').classList.add('traslate')
-        document.querySelector('.fondo2').style.width = "100%"
-    document.querySelector('.fondo1').classList.add('scaleFondo')
+        fondo2.classList.add('traslate')
+        fondo2.style.width = "100%"
+    fondo1.classList.add('scaleFondo')
     },5)
-    
     setTimeout(function () {
-        document.querySelector('.fondo3').style.display = ""
-        document.querySelector('.fondo2').style.filter = "grayscale(100%)"
-        document.querySelector('.fondo1').classList.remove('scaleFondo')
+        fondo1.classList.remove('scaleFondo')
+        fondo3.style.display = ""
+        fondo2.style.filter = "grayscale(100%)"
     },3000)  
 })
