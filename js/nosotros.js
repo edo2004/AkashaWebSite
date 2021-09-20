4a6d35bafd7533d53a517a9bc971f7bc30a12802
Eduar <eduarord@unicauca.edu.co>
2021-09-20 17:29:59 -0500

    Nosotros completo

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,93 +1,88 @@
 const textDescription = document.querySelector('.text-description')
 const fondoNosotros = document.querySelector('.fondo-nosotros')
-const templateNosotros = document.querySelector('#template-nosotros').content;
 const fragment1 = document.createDocumentFragment();
-
-// textDescription.innerHTML = ""
 
 const ContentGifs = [
     {
-        description:`<h1>Nosotros</h1>
-        <p class="hola-mundo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>
-        <img src="./assets/LogoFooter.svg" alt="LogoFooter">`
+        description:`<h1>Motion Graphics</h1>
+        <p class="hola-mundo">Damos vida a tus ideas, mezclándolas con gráficos en movimiento para expresarlas de manera dinámica, atractiva y emocionante.</p>
+        <p class="hola-mundo">No hay límites en lo que podamos crear, es la mejor manera de capturar la atención de tu público en:</p>
+        <ul>
+            <li>Redes sociales</li>
+            <li>Comerciales</li>
+            <li>Infografías.</li>
+            <li>Videos institucionales y Educativos</li>
+        </ul>
+        <p>¡Y mucho más!</p>
+        <img src="./assets/gifs/gif_Motion_graphics.gif" alt="LogoFooter">`
+    },
+    {
+        description:`<h1>Animatics y Storyboards</h1>
+        <p class="hola-mundo">Te damos un primer vistazo de tu idea audiovisual para que puedas revisarla y modificarla antes de que llegue a filmarse o animarse en tus proyectos de:</p>
+        <ul>
+            <li>Comerciales</li>
+            <li>Cine</li>
+            <li>TV</li>
+            <li>Videos institucionales y educativos</li>
+        </ul>
+        <img src="./assets/gifs/gif_Cinematics.gif" alt="LogoFooter">`
     },
     {
         description:`<h1>Nosotros</h1>
-        <p class="hola-mundo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>
+        <p class="hola-mundo">Somos un estudio digital donde creamos productos audiovisuales que cuentan historias, resuelven problemas y dan de qué hablar.</p>
+        <p class="hola-mundo">Buscamos los medios para llegar a hacer realidad diferentes ideas, nuevas formas de hacer reír, emocionar y sorprender. Estamos explorando el universo de la animación para satisfacer el mercado.</p>
         <img src="./assets/LogoFooter.svg" alt="LogoFooter">`
     },
     {
-        description:`<h1>Nosotros</h1>
-        <p class="hola-mundo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>
+        description:`<h1>Visualización Arquitectónica</h1>
+        <p class="hola-mundo">Proyectamos tus ideas inmobiliarias en imágenes y animaciones digitales para mejorar el marketing y la promoción de tus proyectos.</p>
+        <ul>
+            <li>Imágenes</li>
+            <li>Recorridos Animados</li>
+            <li>Panoramicas 360</li>
+        </ul>
         <img src="./assets/LogoFooter.svg" alt="LogoFooter">`
     },
     {
-        description:`<h1>Nosotros</h1>
-        <p class="hola-mundo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>
-        <img src="./assets/LogoFooter.svg" alt="LogoFooter">`
+        description:`<h1>Animatics y Storyboards</h1>
+        <p class="hola-mundo">Resuelve lo que creías complicado de expresar con imágenes bidimensionales en movimiento mediante la técnica CutOut digital aplicado a:</p>
+        <ul>
+            <li>Infografías</li>
+            <li>Personajes</li>
+            <li>Redes Sociales</li>
+            <li>Textos</li>
+        </ul>
+        <img src="./assets/gifs/gif_Animacion2D.gif" alt="animation-2d">`
     },
     {
-        description:`<h1>Nosotros</h1>
-        <p class="hola-mundo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>
-        <img src="./assets/LogoFooter.svg" alt="LogoFooter">`
+        description:`<h1>Animatics y Storyboards</h1>
+        <p class="hola-mundo">Soluciona lo que creías imposible de lograr con un entorno tridimensional desde polígonos sencillos hasta fluidos y dinámicas aplicadas a:</p>
+        <ul>
+            <li>Demos 3D</li>
+            <li>Personajes</li>
+            <li>Infografías 3D</li>
+            <li>Simulaciones físicas</li>
+            <li>Integración Live Action</li>
+        </ul>
+        <img src="./assets/gifs/gif_Animacion3D.gif" alt="LogoFooter">`
     }
 ]
 
-console.log(ContentGifs[0])
-// if (textDescription.innerHTML == '') {
-//     textDescription.innerHTML = `<iframe class="video-galery" src="https://player.vimeo.com/video/583627240?title=0&dnt=0&byline=0&playsinline=0" frameborder="0" allow="autoplay"; fullscreen" allowfullscreen></iframe>`
-// }
 
 function cargarGifs(data) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '../nosotros.json',true);
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200){
-            let datos = JSON.parse(this.responseText)
-            console.log(data)
-            var idImagen = data.dataset.id
-            console.log(idImagen)
-            textDescription.innerHTML = "";
-            textDescription.classList.add("visible")
-            
-            datos.forEach(item => {
-                if(idImagen == item.id){
-                    // console.log(item)
-                    let description = item.description;
-                    let imagen = item.image
-                    console.log(imagen)
-                    setTimeout(function () {
 
-                        if(item.id === "2")
-
-                        textDescription.innerHTML =  ContentGifs[0].description
-                        textDescription.classList.remove("visible")
-                    },500)
-                    // templateNosotros.querySelector('p').textContent = description
-                    // templateNosotros.querySelector('img').setAttribute("src", imagen);
-
-                    // const clonar = templateNosotros.cloneNode(true);
-                    // fragment1.appendChild(clonar);
-                }
-            });
-            // textDescription.appendChild(fragment1)
-            
-        }
-    }
-}
-function cargarHome() {
-    setTimeout(function() {
-        textDescription.classList.add("visible")
-    },10)
+    var idImagen = data.dataset.id
+    textDescription.innerHTML = "";
+    textDescription.classList.add("visible")
+    
     setTimeout(function () {
+
+        textDescription.innerHTML =  ContentGifs[idImagen-1].description
         textDescription.classList.remove("visible")
-        textDescription.innerHTML = "";
-        textDescription.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eum ut laudantium. Reiciendis, repellendus delectus! Autem sint officia perspiciatis pariatur harum commodi, reiciendis eveniet nesciunt nostrum, saepe aspernatur sapiente fuga.</p>`
-    },500)  
-    
-    
+    },500)
+            
 }
+
 
 // efecto parallax
 
@@ -114,6 +109,10 @@ function parallax(e){
 
     })
 }
+
+
+
+
 
 // Todo iba bien hasta cuando...xD
 // let Semana = ['Lunes', 'Martes', 'Jueves', 'Viernes','Sabado', 'Domingo'];
